21ad111752fc0a9cce34e1d716c061745b77d569
Thanushika Kunananthan <cst20046@std.uwu.ac.lk>
2024-06-17 15:54:41 +0530

    side bar header 1

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,36 +3,44 @@ import './Sidebar.css'
 import DonutLargelcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Avatar,IconButton } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 
 
 function Sidebar() {
-  return (
-    <div className='sidebar'>
-<div className='sidebar_header'>
-    <Avatar/>
-    <div className='sidebar_headerRight'>
-        <IconButton>
-    <DonutLargelcon/>
-    </IconButton>
-    <IconButton>
-    <ChatIcon/>
-    </IconButton>
-    <IconButton>
-    <MoreVertIcon/>
-    </IconButton>
-   
-    </div>
-    </div>    
-    <div className='sidebar_search'>
+    return (
+        <div className='sidebar'>
+            <div className='sidebar_header'>
+                <Avatar />
+                <div className='sidebar_headerRight'>
+                    <IconButton>
+                        <DonutLargelcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
 
-    </div>
-    <div className='sidebar_chats'>
+                </div>
+            </div>
+            <div className='sidebar_search'>
+                <div className='sidebar_searchContainer'>
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <input placeholder='Search or start new chat' type='text' />
+                </div>
 
-    </div>
-    </div>
-  )
+            </div>
+            <div className='sidebar_chats'>
+
+            </div>
+        </div>
+    )
 }
 
 export default Sidebar
